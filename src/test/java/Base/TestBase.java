c4d7d97e300282fae9c5361f9592024e19e6aba6
RITESH RANJAN MISHRA <58725105+ritesh2195@users.noreply.github.com>
2020-02-10 18:54:56 +0530

    Update TestBase.java

--- src/test/java/Base/TestBase.java
+++ src/test/java/Base/TestBase.java
@@ -1,18 +1,16 @@
  package Base;
 
-	import org.apache.log4j.Level;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.BeforeClass;
-
 import io.restassured.response.Response;
-	import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.RequestSpecification;
 	
-	public class TestBase {
-
-	public static RequestSpecification httpRequest;
-	public static Response response;
-	public String empID="21";
+    public class TestBase {
+    public static RequestSpecification httpRequest;
+    public static Response response;
+    public String empID="21";
 	
     public Logger logger;
 	  
